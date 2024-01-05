81d8c94ce04c1676b3bfc28859a488617048f222
WillBoudreau <williamboudreau804@gmail.com>
2024-01-05 11:15:07 -0500

    Created the Project, January 5th 2024

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldButBetter_WillB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hello World");
+            Console.WriteLine("Press any key to deiplay the message");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
